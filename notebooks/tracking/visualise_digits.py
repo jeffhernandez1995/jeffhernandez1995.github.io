a87ab42222ce4769140430a4f44808a533a6b33b
jefehern <jefehern@pop-os.localdomain>
2020-01-19 20:11:23 -0500

    new more challenging dataset

--- notebooks/tracking/visualise_digits.py
+++ notebooks/tracking/visualise_digits.py
@@ -1,5 +1,4 @@
 import numpy as np
-from datasets import MovingMNIST
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -9,32 +8,32 @@ mpl.rcParams['savefig.pad_inches'] = 0
 # mpl.rcParams['animation.html'] = 'html5'
 
 
-num_digits = 5
-digits = np.array([0, 2, 4, 6, 8])
-seq_len = 100
+seq = np.load('data/mnist_training_fast_videos.npy')[0]
+bbox = trajectory_tensor = np.load('data/mnist_training_fast_trajectories.npy')[0]
+bbox = bbox.swapaxes(0, 1)
+bbox[:, :, 1] = seq.shape[1] - bbox[:, :, 1]
+bbox[:, :, 3] = seq.shape[1] - bbox[:, :, 3]
 
-dset = MovingMNIST(True, seq_len=seq_len, num_digits=num_digits, digits=digits)
-
-seq, bbox = dset[0]
 
 def plot_gt(seq):
     fig = plt.figure(frameon=False)
     ax = plt.Axes(fig, [0., 0., 1., 1.])
     ax.set_axis_off()
     fig.add_axes(ax)
+
     def update(i):
-        im.set_data(seq[i, :, :, 0])
+        im.set_data(seq[i, :, :])
         for j in range(bbox.shape[1]):
             x, y = bbox[i, j, 0], bbox[i, j, 1]
-            w, h = 28, 28
+            w, h = bbox[i, j, 2] - bbox[i, j, 0], bbox[i, j, 3] - bbox[i, j, 1]
             rects[j].set_width(w)
             rects[j].set_height(h)
             rects[j].set_xy([x, y])
-    im = ax.imshow(seq[0, :, :, 0], cmap='gray', aspect='auto')
+    im = ax.imshow(seq[0, :, :], cmap='gray', aspect='auto')
     rects = [0]*bbox.shape[1]
     for j in range(bbox.shape[1]):
         x, y = bbox[0, j, 0], bbox[0, j, 1]
-        w, h = 28, 28
+        w, h = bbox[0, j, 2] - bbox[0, j, 0], bbox[0, j, 3] - bbox[0, j, 1]
         rects[j] = patches.Rectangle((x, y), w, h, linewidth=1, edgecolor='r', facecolor='none')
         ax.add_patch(rects[j])
     ax.set_xticks([])
@@ -45,11 +44,12 @@ def plot_gt(seq):
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
     ani = animation.FuncAnimation(fig,
-                              update,
-                              seq.shape[0],
-                              interval=1000/5,
-                              blit=False)
+                                  update,
+                                  seq.shape[0],
+                                  interval=1000/5,
+                                  blit=False)
     return ani
+
 
 Writer = animation.writers['ffmpeg']
 writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
