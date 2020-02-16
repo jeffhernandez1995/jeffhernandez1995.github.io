d8cdd7f67b8e02f1c3f6eb7e2e486e4d0f32e803
jefehern <jefehern@pop-os.localdomain>
2020-02-16 18:09:26 -0500

    finish trackign post

--- notebooks/tracking/test_relational_unkown.py
+++ notebooks/tracking/test_relational_unkown.py
@@ -4,9 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset, DataLoader
 from lapsolver import solve_dense
-from datasets import AssociationMNIST
 from losses import AssociationLoss
 from models import InteractionNet
 
@@ -19,10 +18,20 @@ def encode_onehot(labels):
                              dtype=np.int32)
     return labels_onehot
 
-digits = np.array([1, 3, 5, 7, 9])
-digit_size = 28
-train_dataset = AssociationMNIST(True, digits=digits)
-test_dataset = AssociationMNIST(False, digits=digits)
+digits = np.array([0, 2, 4, 6, 8])
+
+X_train = np.load('datasets/X_train.npy')
+y_train = np.load('datasets/y_train.npy')
+X_test = np.load('datasets/X_test.npy')
+y_test = np.load('datasets/y_test.npy')
+
+X_train = torch.from_numpy(X_train).float()
+y_train = torch.from_numpy(y_train).float()
+X_test = torch.from_numpy(X_test).float()
+y_test = torch.from_numpy(y_test).float()
+
+train_dataset = TensorDataset(X_train, y_train)
+test_dataset = TensorDataset(X_test, y_test)
 
 cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if cuda else "cpu")
@@ -43,10 +52,10 @@ if device:
     rel_rec = rel_rec.to(device)
     rel_send = rel_send.to(device)
 
-model = InteractionNet(256, 256*2, digits.shape[0], n_iter, 0.5)
+model = InteractionNet(28*28, 256*2, digits.shape[0], n_iter, 0.25)
 if device:
     model.to(device)
-model.load_state_dict(torch.load('models/InteractionNet__29_loss=0.01199802.pth'))
+model.load_state_dict(torch.load('models/InteractionNet__11_loss=0.2297398.pth'))
 weights = [1/3]*3
 model.eval()
 criterion = AssociationLoss(weights)
@@ -54,20 +63,50 @@ softmax = nn.Softmax(dim=1)
 n_digits = 3
 
 with torch.no_grad():
-    for i, (inp, target) in enumerate(train_loader):
+    for i, (inp, target) in enumerate(test_loader):
         if device:
             inp = inp.to(device)
             target = target.to(device)
         # inp[0, 4, 0, :] = torch.zeros(256, device=device) # (5, 256)
+
         y_pred = model(inp, rel_rec, rel_send)
         loss = criterion(y_pred, target)
         y_pred_ = softmax(y_pred[0])
+        matrix = y_pred_.to('cpu').detach().numpy()
         y_pred_ = (y_pred_ * 10**n_digits).round() / (10**n_digits)
-        matrix = y_pred[0].to('cpu').detach().numpy()
         rids, cids = solve_dense(-matrix)
         matched_indices = np.array([rids, cids]).T
-        print(matched_indices)
-        print(target[0])
-        print(loss.item())
+        fig, ax = plt.subplots(nrows=5, ncols=2)
+        for i, row in enumerate(ax):
+            for j, col in enumerate(row):
+                r = matched_indices[i, j]
+                img = inp[0, r, j, :, :].to('cpu').detach().numpy()
+                col.imshow(img, cmap='gray', aspect='auto')
+                col.set_axis_off()
+                col.set_xticks([])
+                col.set_yticks([])
+                col.get_xaxis().set_ticklabels([])
+                col.get_yaxis().set_ticklabels([])
+                col.get_xaxis().set_visible(False)
+                col.get_yaxis().set_visible(False)
+        plt.show()
+        plt.close()
+        rids, cids = solve_dense(-target[0].to('cpu').detach().numpy())
+        matched_indices = np.array([rids, cids]).T
+        fig, ax = plt.subplots(nrows=5, ncols=2)
+        for i, row in enumerate(ax):
+            for j, col in enumerate(row):
+                r = matched_indices[i, j]
+                img = inp[0, r, j, :, :].to('cpu').detach().numpy()
+                col.imshow(img, cmap='gray', aspect='auto')
+                col.set_axis_off()
+                col.set_xticks([])
+                col.set_yticks([])
+                col.get_xaxis().set_ticklabels([])
+                col.get_yaxis().set_ticklabels([])
+                col.get_xaxis().set_visible(False)
+                col.get_yaxis().set_visible(False)
+        plt.show()
+        plt.close()
+
         assert 2 == 1
-    
--- notebooks/tracking/visualise_digits.py
+++ notebooks/tracking/visualise_digits.py
@@ -4,15 +4,100 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.patches as patches
 from matplotlib import rc
+from sort import Sort
 mpl.rcParams['savefig.pad_inches'] = 0
 # mpl.rcParams['animation.html'] = 'html5'
 
 
-seq = np.load('data/mnist_training_fast_videos.npy')[0]
-bbox = trajectory_tensor = np.load('data/mnist_training_fast_trajectories.npy')[0]
+# seq = np.load('datasets/X_train.npy')[0].reshape(10, 28, 28)
+
+# for i in range(10):
+#     fig = plt.figure(frameon=False)
+#     ax = plt.Axes(fig, [0., 0., 1., 1.])
+#     ax.set_axis_off()
+#     fig.add_axes(ax)
+#     im = ax.imshow(seq[i, :, :], cmap='gray', aspect='auto')
+#     ax.set_xticks([])
+#     ax.set_yticks([])
+
+#     ax.get_xaxis().set_ticklabels([])
+#     ax.get_yaxis().set_ticklabels([])
+#     ax.get_xaxis().set_visible(False)
+#     ax.get_yaxis().set_visible(False)
+#     fig.savefig(f'resources/fig_{i}.pdf')
+#     plt.close()
+
+# seq = np.load('data/mnist_training_fast_videos.npy')[0]
+# bbox = trajectory_tensor = np.load('data/mnist_training_fast_trajectories.npy')[0]
+# bbox = bbox.swapaxes(0, 1)
+# bbox[:, :, 1] = seq.shape[1] - bbox[:, :, 1]
+# bbox[:, :, 3] = seq.shape[1] - bbox[:, :, 3]
+
+
+# def plot_gt(seq):
+#     fig = plt.figure(frameon=False)
+#     ax = plt.Axes(fig, [0., 0., 1., 1.])
+#     ax.set_axis_off()
+#     fig.add_axes(ax)
+
+#     def update(i):
+#         im.set_data(seq[i, :, :])
+#         for j in range(bbox.shape[1]):
+#             x, y = bbox[i, j, 0], bbox[i, j, 1]
+#             w, h = bbox[i, j, 2] - bbox[i, j, 0], bbox[i, j, 3] - bbox[i, j, 1]
+#             rects[j].set_width(w)
+#             rects[j].set_height(h)
+#             rects[j].set_xy([x, y])
+#     im = ax.imshow(seq[0, :, :], cmap='gray', aspect='auto')
+#     rects = [0]*bbox.shape[1]
+#     for j in range(bbox.shape[1]):
+#         x, y = bbox[0, j, 0], bbox[0, j, 1]
+#         w, h = bbox[0, j, 2] - bbox[0, j, 0], bbox[0, j, 3] - bbox[0, j, 1]
+#         rects[j] = patches.Rectangle((x, y), w, h, linewidth=1, edgecolor='r', facecolor='none')
+#         ax.add_patch(rects[j])
+#     ax.set_xticks([])
+#     ax.set_yticks([])
+
+#     ax.get_xaxis().set_ticklabels([])
+#     ax.get_yaxis().set_ticklabels([])
+#     ax.get_xaxis().set_visible(False)
+#     ax.get_yaxis().set_visible(False)
+#     ani = animation.FuncAnimation(fig,
+#                                   update,
+#                                   seq.shape[0],
+#                                   interval=1000/5,
+#                                   blit=False)
+#     return ani
+
+
+# Writer = animation.writers['ffmpeg']
+# writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+
+# ani = plot_gt(seq)
+# ani.save('anim.mp4', writer=writer)
+
+
+seq = np.load('data/icons8_testing_fast_videos.npy')[0]
+bbox = np.load('data/icons8_testing_fast_trajectories.npy')[0]
 bbox = bbox.swapaxes(0, 1)
 bbox[:, :, 1] = seq.shape[1] - bbox[:, :, 1]
 bbox[:, :, 3] = seq.shape[1] - bbox[:, :, 3]
+ids = np.repeat(np.arange(5), 100).reshape(5, 100, 1)
+ids = ids.swapaxes(0, 1)
+bbox = np.concatenate((bbox, ids), axis=2)
+
+tracker = Sort()
+trks = np.zeros(bbox.shape)
+for t in range(bbox.shape[0]):
+    dets = bbox[t].copy()
+    dets[:, -1] = 1
+    trks_ = tracker.update(dets)
+    trks_[:, 2] = trks_[:, 2] - trks_[:, 0]
+    trks_[:, 3] = trks_[:, 3] - trks_[:, 1]
+    trks[t, :trks_.shape[0], :] = trks_
+print(np.unique(trks[:, :, 4].flatten()))
+assert 2 == 1
+colors = ['r', 'g', 'b', 'c', 'm']
 
 
 def plot_gt(seq):
